8585d626cd0ebc975febb37ba5057b42ee0fc8fd
Shuri <julian.garrocho712@gmail.com>
2024-07-27 04:07:20 -0300

    Update layout using Bootstrap
    - I've replaced the first layout's design for a simpler card-based one
    using Bootstrap.

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,32 +1,50 @@
 function displayPokemonList() {
-  const $pokemonList = document.querySelector("#pokemon-list");
-
   getPokemonList(0, 30).then((list) => {
-    const pokemonList = list.results;
-
-    Object.keys(pokemonList).forEach((key) => {
-      // Regex pattern to get the Pokemon's ID from the URL
-      // Example: https://pokeapi.co/api/v2/pokemon/74/
-      const idPattern = /\d+\/$/;
-      const pokemonId = pokemonList[key].url
-        .match(idPattern)[0]
-        .replace("/", "");
-
-      const spriteDefault = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonId}.png`;
-
-      $pokemonList.insertAdjacentHTML(
-        "beforeend",
-        `
-          <div class="pokemon-container">
-            <img 
-              src="${spriteDefault}"
-              class="pokemon-img"
-              height="100"
-              width="100"
-            >
-          </div>
-        `
-      );
+    const pokemonList = formatPokemonList(list.results);
+    const $pokemonContainer = document.querySelector("#pokemon-list");
+    Object.values(pokemonList).forEach((pokemon) => {
+      createPokemonElement(pokemon, $pokemonContainer);
     });
   });
+}
+
+function formatPokemonList(pokemonList) {
+  return pokemonList.map((pokemon) => {
+    const idMatch = pokemon.url.match(/\d+\/$/);
+    const pokemonId = idMatch ? idMatch[0].replace("/", "") : null;
+    const pokemonName = pokemon.name;
+    return {
+      name: pokemonName,
+      id: Number(pokemonId),
+    };
+  });
+}
+
+function createPokemonElement(pokemon, pokemonContainer) {
+  const $pokemonEl = document.createElement("div");
+  $pokemonEl.classList.add("pokemon-box");
+  $pokemonEl.id = "pokemon-box";
+
+  $pokemonEl.insertAdjacentHTML(
+    "beforeend",
+    `
+      <p class="pokemon-id m-0">#${pokemon.id}</p>
+      <img 
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" 
+        class="img-fluid pokemon-img"
+        alt="pokemon sprite"
+        aria-label="Image of ${pokemon.name}"
+        data-name="${pokemon.name}"
+        data-id="${pokemon.id}"
+        title="${pokemon.name}"
+        height=100
+        width=100
+      >
+      <p class="pokemon-name m-0 mt-1">
+        ${pokemon.name}
+      </p>
+    `
+  );
+
+  pokemonContainer.appendChild($pokemonEl);
 }
