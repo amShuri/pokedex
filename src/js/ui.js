f04580a18c55f6bf69c62304d20f2f1d27eb3736
Shuri <julian.garrocho712@gmail.com>
2024-07-28 21:50:07 -0300

    Rename `pokemonId` to `pokemonNumber` for clarity
    - This is a minor change that prepares the code for the implementation
    of modals. The name "pokemonId" could be confusing so I'm using
    pokemonNumber instead.

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,19 +1,3 @@
-function displayPokemonList(offset) {
-  getPokemonList(offset).then((list) => {
-    const pokemonList = formatPokemonList(list.results);
-    const $pokemonContainer = document.querySelector("#pokemon-list");
-    $pokemonContainer.innerHTML = "";
-
-    Object.values(pokemonList).forEach((pokemon) => {
-      createPokemonElement(pokemon, $pokemonContainer);
-    });
-
-    updatePreviousPageButton(list.previous);
-    updateNextPageButton(list.next);
-    updatePageCount(list.count);
-  });
-}
-
 function createPokemonElement(pokemon, pokemonContainer) {
   const $pokemonEl = document.createElement("div");
   $pokemonEl.classList.add("pokemon-box");
@@ -22,14 +6,14 @@ function createPokemonElement(pokemon, pokemonContainer) {
   $pokemonEl.insertAdjacentHTML(
     "beforeend",
     `
-      <p class="pokemon-id m-0">#${pokemon.id}</p>
+      <p class="pokemon-number m-0">#${pokemon.number}</p>
       <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" 
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.number}.png" 
         class="img-fluid pokemon-img"
         alt="pokemon sprite"
         aria-label="Image of ${pokemon.name}"
-        data-name="${pokemon.name}"
-        data-id="${pokemon.id}"
+        data-pokemon-name="${pokemon.name}"
+        data-pokemon-number="${pokemon.number}"
         title="${pokemon.name}"
         height=100
         width=100
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,27 @@
+function displayPokemonList(offset) {
+  getPokemonList(offset).then((list) => {
+    const pokemonList = formatPokemonList(list.results);
+    const $pokemonContainer = document.querySelector("#pokemon-list");
+    $pokemonContainer.innerHTML = "";
+
+    Object.values(pokemonList).forEach((pokemon) => {
+      createPokemonElement(pokemon, $pokemonContainer);
+    });
+
+    updatePreviousPageButton(list.previous);
+    updateNextPageButton(list.next);
+    updatePageCount(list.count);
+  });
+}
+
 function formatPokemonList(pokemonList) {
   return pokemonList.map((pokemon) => {
     const idMatch = pokemon.url.match(/\d+\/$/);
-    const pokemonId = idMatch ? idMatch[0].replace("/", "") : null;
+    const pokemonNumber = idMatch ? idMatch[0].replace("/", "") : null;
     const pokemonName = pokemon.name;
     return {
       name: pokemonName,
-      id: Number(pokemonId),
+      number: Number(pokemonNumber),
     };
   });
 }
